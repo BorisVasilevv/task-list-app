142fc7207c46b089abc3d6a755cf6a2c9a9f313d
Boris <BacR009@yandex.ru>
2023-12-27 00:42:53 +0500

    p

--- src/components/AddTask/AddNewTaskElem.js
+++ src/components/AddTask/AddNewTaskElem.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import {phoneMarks, phoneModels} from "./constants";
 import {useState} from "react";
 import {useAddNewPhone} from "../../hooks/useTasks";
 
@@ -15,68 +14,20 @@ export const AddNewPhone = () => {
     const {addNewPhone: addNewPhoneToLS} = useAddNewPhone()
 
     const [name, setName] = useState('');
-    const [phoneMark, setPhoneMark] = useState('');
-    const [phoneModel, setPhoneModel] = useState('');
 
     const onInputChange = (event) => {
         setName(event.nativeEvent.target.value);
     }
 
-    const onPhoneMarkSelected = (event) => {
-        setPhoneMark(event.nativeEvent.target.value);
-        setPhoneModel('');
-    }
-
-    const onPhoneModelSelected = (event) => {
-        setPhoneModel(event.nativeEvent.target.value);
-    }
-
-    const renderPhoneMarksOptions = () => {
-        return phoneMarks.map((el, index) => {
-            return (
-                <option key={index} value={el}>
-                    {el}
-                </option>
-            );
-        })
-
-    }
-
-    const renderPhoneModelsOptions = () => {
-        if (!phoneMark) {
-            return null;
-        }
-        const models = phoneModels[phoneMark];
-        if (models) {
-            return models.map((el, index) => {
-               return (
-                   <option key={index} value={el}>
-                       {el}
-                   </option>
-               );
-            });
-        }
-    }
-
     const addNewPhone = () => {
         addNewPhoneToLS({
-            name,
-            phoneMark,
-            phoneModel
+            name
         });
     }
 
     return (
         <Container>
-            <input value={name} onChange={onInputChange} placeholder={'Имя владельца'} />
-            <select onChange={onPhoneMarkSelected} value={phoneMark}>
-                <option key={'default'} disabled={true} value={''}>Марка телефона</option>
-                {renderPhoneMarksOptions()}
-            </select>
-            <select onChange={onPhoneModelSelected} value={phoneModel}>
-                <option key={'default'} disabled={true} value={''}>Модель телефона</option>
-                {renderPhoneModelsOptions()}
-            </select>
+            <input value={name} onChange={onInputChange} placeholder={'Задача'} />
             <button onClick={addNewPhone}>Сохранить</button>
         </Container>
     )
