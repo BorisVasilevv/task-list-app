ce8bb2089ba2aa1739da8f3c514bd775e0e4ca9f
Boris <BacR009@yandex.ru>
2023-12-27 11:48:51 +0500

    addid

--- src/dataActions/Task.js
+++ src/dataActions/Task.js
@@ -1,13 +1,21 @@
 export const TaskActions = {
     saveValue: (value) => {
         return new Promise((resolve, reject) => {
-            let valueToSave = value;
-            if (!value) {
-                valueToSave = [];
-            }
-            localStorage.setItem('tasks', JSON.stringify(valueToSave));
-            resolve();
-        })
+            TaskActions.getValue().then((tasks) => {
+                let maxId = 0;
+                if (!value) {
+                    valueToSave = [];
+                }
+                
+                if (tasks.length){
+                    maxId = Math.max(...tasks.map(t => t.id));
+                }
+                let valueToSave = {"name": value[maxId].name, "id":maxId};
+                console.log(value[0].name);
+                localStorage.setItem('tasks', JSON.stringify(valueToSave));
+                resolve();
+            })
+        }).catch();
     },
     getValue: () => {
         return new Promise((resolve, reject) => {
